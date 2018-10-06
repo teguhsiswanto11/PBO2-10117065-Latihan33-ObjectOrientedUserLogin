ed50934846218ad26db2eda65f9f5c42e48b79f3
Teguh Siswanto <tsiswanto73@gmail.com>
2018-10-06 22:54:08 +0700

    Initial commit

---
+++ src/Latihan33Login.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * @author
+ * NAMA     : Teguh Siswanto
+ * KELAS    : PBO2
+ * NIM      : 10117065
+ * Deskripsi Program : memasukkan username dan password untuk login,
+ * memberikan alert jika terjadi kesalahan login
+ *
+ */
+public class Latihan33Login {
+    private static String usName;
+    private static String passWord;
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Masukkan Username = ");
+        usName = scn.next();
+
+        System.out.print("Masukkan Password = ");
+        passWord = scn.next();
+
+        User cek = new User();
+        cek.pengecekkanLogin(usName,passWord);
+
+    }
+
+}
